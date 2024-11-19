7bf62bae2bde9deb1dbc42f91ee21b6ea13703e6
Shrey-akan <shereyansjain0904@gmail.com>
2024-11-19 18:53:38 +0530

    update 19-11-2024

--- src/app/dashboardemp/dashboardemp-routing.module.ts
+++ src/app/dashboardemp/dashboardemp-routing.module.ts
@@ -1,68 +1,82 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { HeaderdashboardempComponent } from './headerdashboardemp/headerdashboardemp.component';
-import { PostjobComponent } from './postjob/postjob.component';
-import { AlljobsComponent } from './alljobs/alljobs.component';
-import { ProfilemepComponent } from './profilemep/profilemep.component';
-import { EmpmessageComponent } from './empmessage/empmessage.component';
-import { ApplieduserdetailsComponent } from './applieduserdetails/applieduserdetails.component';
-import { UpdateempprofileComponent } from './updateempprofile/updateempprofile.component';
-import { NotificationComponent } from '../dashboarduser/notification/notification.component';
-import { NotificationempComponent } from './notificationemp/notificationemp.component';
-import { VideocallComponent } from './videocall/videocall.component';
-import { UpdatejobComponent } from './updatejob/updatejob.component';
-import { authGuard } from '../auth.guard';
-import { SetQuestionComponent } from './set-question/set-question.component';
-import { DisapprovejobComponent } from './disapprovejob/disapprovejob.component';
-import { UpdatedisjobComponent } from './updatedisjob/updatedisjob.component';
-
-
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { HeaderdashboardempComponent } from "./headerdashboardemp/headerdashboardemp.component";
+import { PostjobComponent } from "./postjob/postjob.component";
+import { AlljobsComponent } from "./alljobs/alljobs.component";
+import { ProfilemepComponent } from "./profilemep/profilemep.component";
+import { EmpmessageComponent } from "./empmessage/empmessage.component";
+import { ApplieduserdetailsComponent } from "./applieduserdetails/applieduserdetails.component";
+import { UpdateempprofileComponent } from "./updateempprofile/updateempprofile.component";
+import { NotificationComponent } from "../dashboarduser/notification/notification.component";
+import { NotificationempComponent } from "./notificationemp/notificationemp.component";
+import { VideocallComponent } from "./videocall/videocall.component";
+import { UpdatejobComponent } from "./updatejob/updatejob.component";
+import { authGuard } from "../auth.guard";
+import { SetQuestionComponent } from "./set-question/set-question.component";
+import { DisapprovejobComponent } from "./disapprovejob/disapprovejob.component";
+import { UpdatedisjobComponent } from "./updatedisjob/updatedisjob.component";
 
 const routes: Routes = [
   {
-    path: '', component: HeaderdashboardempComponent,
-    canActivate: [authGuard], 
+    path: "",
+    component: HeaderdashboardempComponent,
+    canActivate: [authGuard],
     children: [
       {
-        path: '', component: PostjobComponent
-      }, {
-        path: 'alljobs', component: AlljobsComponent
-      }, {
-        path: 'profilemep', component: ProfilemepComponent
-      }, {
-        path: 'empmessage/:uid', component: EmpmessageComponent
+        path: "",
+        component: PostjobComponent,
       },
       {
-        path: 'applieduserdetails', component: ApplieduserdetailsComponent
+        path: "alljobs",
+        component: AlljobsComponent,
       },
       {
-        path: 'updateempprofile/:empid', component: UpdateempprofileComponent
+        path: "profilemep",
+        component: ProfilemepComponent,
       },
       {
-        path: 'notificationemp', component: NotificationempComponent
+        path: "empmessage/:uid",
+        component: EmpmessageComponent,
       },
-{
-path:'set-question/:jobid',component:SetQuestionComponent
-},
-{
-  path:'disapprovejob',component:DisapprovejobComponent
-  },
+      {
+        path: "applieduserdetails",
+        component: ApplieduserdetailsComponent,
+      },
+      {
+        path: "updateempprofile/:empid",
+        component: UpdateempprofileComponent,
+      },
+      {
+        path: "notificationemp",
+        component: NotificationempComponent,
+      },
+      {
+        path: "set-question/:jobid",
+        component: SetQuestionComponent,
+      },
+      {
+        path: "disapprovejob",
+        component: DisapprovejobComponent,
+      },
 
       {
-        path:'videocall/:uid', component:VideocallComponent
+        path: "videocall/:uid",
+        component: VideocallComponent,
       },
       {
-        path:'updatejob/:jobid',component:UpdatejobComponent
+        path: "updatejob/:jobid",
+        component: UpdatejobComponent,
       },
       {
-        path:'updatedisjob/:jobid',component:UpdatedisjobComponent
-      }
-    ]
-  }
+        path: "updatedisjob/:jobid",
+        component: UpdatedisjobComponent,
+      },
+    ],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class DashboardempRoutingModule { }
+export class DashboardempRoutingModule {}
--- src/app/dashboardemp/videocall/videocall.component.ts
+++ src/app/dashboardemp/videocall/videocall.component.ts
@@ -1,14 +1,18 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
-import Peer from 'peerjs';
-import { FormBuilder, Validators } from '@angular/forms';
-import { ActivatedRoute , Router } from '@angular/router';
-import { CookieService } from 'ngx-cookie-service';
-import { backendUrl } from 'src/app/constant';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { io, Socket } from 'socket.io-client';
-
-
+import { HttpClient } from "@angular/common/http";
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  Renderer2,
+  ViewChild,
+} from "@angular/core";
+import Peer from "peerjs";
+import { FormBuilder, Validators } from "@angular/forms";
+import { ActivatedRoute, Router } from "@angular/router";
+import { CookieService } from "ngx-cookie-service";
+import { backendUrl } from "src/app/constant";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { io, Socket } from "socket.io-client";
 
 class SendMessage {
   messageTo!: string;
@@ -16,50 +20,59 @@ class SendMessage {
   message!: string;
 }
 @Component({
-  selector: 'app-videocall',
-  templateUrl: './videocall.component.html',
-  styleUrls: ['./videocall.component.css']
+  selector: "app-videocall",
+  templateUrl: "./videocall.component.html",
+  styleUrls: ["./videocall.component.css"],
 })
 export class VideocallComponent implements OnInit {
-
   message: SendMessage = new SendMessage(); // Initialize an empty message
   uid!: string | null;
- socket!: Socket;
+  socket!: Socket;
   empid!: string;
 
   messageForm!: any;
   messages!: SendMessage[];
   private backend_URL = `${backendUrl}`;
-  @ViewChild('roomInput', { static: true }) roomInput!: ElementRef<HTMLInputElement>;
-  @ViewChild('localVideo', { static: true }) localVideo!: ElementRef<HTMLVideoElement>;
-  @ViewChild('remoteVideo', { static: true }) remoteVideo!: ElementRef<HTMLVideoElement>;
-  @ViewChild('notification', { static: true }) notification!: ElementRef<HTMLElement>;
-  @ViewChild('entryModal', { static: true }) entryModal!: ElementRef<HTMLElement>;
+  @ViewChild("roomInput", { static: true })
+  roomInput!: ElementRef<HTMLInputElement>;
+  @ViewChild("localVideo", { static: true })
+  localVideo!: ElementRef<HTMLVideoElement>;
+  @ViewChild("remoteVideo", { static: true })
+  remoteVideo!: ElementRef<HTMLVideoElement>;
+  @ViewChild("notification", { static: true })
+  notification!: ElementRef<HTMLElement>;
+  @ViewChild("entryModal", { static: true })
+  entryModal!: ElementRef<HTMLElement>;
 
-  PRE = 'Orage';
-  SUF = 'video';
+  PRE = "Orage";
+  SUF = "video";
   room_id!: string;
   local_stream: MediaStream | undefined;
   screenStream: MediaStream | undefined;
   peer: Peer | null = null; // Use Peer from the imported library
   currentPeer: any = null;
   screenSharing = false;
-audioMuted: boolean = false;
+  audioMuted: boolean = false;
   videoMuted: boolean = false;
-  constructor(private snackBar:MatSnackBar, private renderer: Renderer2, private http: HttpClient, private route: ActivatedRoute,
-    private cookie: CookieService, private formBuilder: FormBuilder , private router: Router) {
-	
-}
+  constructor(
+    private snackBar: MatSnackBar,
+    private renderer: Renderer2,
+    private http: HttpClient,
+    private route: ActivatedRoute,
+    private cookie: CookieService,
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {}
   endCall: boolean = false;
 
   ngOnInit(): void {
-    this.empid = this.cookie.get('emp');
-console.log("checking the emmpid",this.empid);
+    this.empid = this.cookie.get("emp");
+    console.log("checking the emmpid", this.empid);
 
     this.uid = this.route.snapshot.paramMap.get("uid");
-    // console.log("uid:", this.uid); 
+    // console.log("uid:", this.uid);
     // Get the "to" value from the cookie (assuming "empemailid" is the cookie name)
-    this.message.messageFrom = this.cookie.get('emp');
+    this.message.messageFrom = this.cookie.get("emp");
     // console.log(this.message.messageFrom);
     // console.log(this.uid);
 
@@ -67,15 +80,14 @@ console.log("checking the emmpid",this.empid);
     this.messageForm = this.formBuilder.group({
       messageFrom: [this.message.messageFrom, Validators.required],
       messageTo: [this.uid, Validators.required],
-      message: [this.message.message, Validators.required]
+      message: [this.message.message, Validators.required],
     });
-    this.loadScript('assets/video-call.js').then(() => {
+    this.loadScript("assets/video-call.js").then(() => {
       // The script has been loaded and executed.
       // You can now call functions and use variables from the script.
     });
 
-  this.initSocketConnection();
-
+    this.initSocketConnection();
   }
   fetchMessages() {
     // Fetch previous messages from the server
@@ -89,7 +101,6 @@ console.log("checking the emmpid",this.empid);
               message.messageFrom === this.message.messageFrom) ||
             (message.messageTo === this.message.messageFrom &&
               message.messageFrom === this.uid)
-
         );
 
         // If relevant messages are found, set the previousMessage field
@@ -101,77 +112,69 @@ console.log("checking the emmpid",this.empid);
       });
   }
 
-
-
-
-
-
   initSocketConnection() {
     // Check if both `empid` and `uid` are available
     if (!this.empid || !this.uid) {
       // console.error('Empid or uid is missing.');
       return;
     }
-  
+
     // Establish socket connection with query parameters
-    this.socket = io('https://job4jobless.in:4400', {
-      transports: ['websocket'],
+    this.socket = io("https://job4jobless.in:4400", {
+      transports: ["websocket"],
       autoConnect: false,
       query: {
         id: this.empid, // Set the empid as sourceId
-        targetId: this.uid ? this.uid : ''   // Set the uid as targetId
-      }
+        targetId: this.uid ? this.uid : "", // Set the uid as targetId
+      },
     });
-  
+
     // Handle socket events
-    this.socket.on('connect_error', (error: any) => {
+    this.socket.on("connect_error", (error: any) => {
       // console.error('Socket Error:', error);
-    });-
-  
-    this.socket.on('message', (message: SendMessage) => {
-      console.log("Message is: ",message);
+    });
+    -this.socket.on("message", (message: SendMessage) => {
+      console.log("Message is: ", message);
       // console.log('Received message:', message);
       this.messages.push(message);
     });
-  
-    this.socket.on('connect', () => {
+
+    this.socket.on("connect", () => {
       // console.log('Socket connected');
       // console.log('Source ID:', this.empid);
       // console.log('Target ID:', this.uid);
     });
-  
+
     // Connect the socket
     this.socket.connect();
   }
-  
- toggleAudio() {
+
+  toggleAudio() {
     this.audioMuted = !this.audioMuted;
     const audioTracks = this.local_stream?.getAudioTracks();
-    audioTracks?.forEach(track => track.enabled = !this.audioMuted);
+    audioTracks?.forEach((track) => (track.enabled = !this.audioMuted));
   }
 
   toggleVideo() {
     this.videoMuted = !this.videoMuted;
     const videoTracks = this.local_stream?.getVideoTracks();
-    videoTracks?.forEach(track => track.enabled = !this.videoMuted);
+    videoTracks?.forEach((track) => (track.enabled = !this.videoMuted));
   }
 
   sendMessage() {
     if (this.messageForm.valid) {
-//      const messageToSend = this.messageForm.value;
-
-
+      //      const messageToSend = this.messageForm.value;
 
       const messageToSend = {
         messageTo: this.uid,
         messageFrom: this.empid,
         message: this.messageForm.value.message,
-//        isSender: this.fullName  // Set isSender based on juname
+        //        isSender: this.fullName  // Set isSender based on juname
       };
 
-       console.log('Sending message:', messageToSend);
+      console.log("Sending message:", messageToSend);
 
-      this.socket.emit('message', messageToSend);
+      this.socket.emit("message", messageToSend);
 
       // Make an HTTP POST request to send the message
       this.http
@@ -181,11 +184,10 @@ console.log("checking the emmpid",this.empid);
             // console.log('Message sent successfully:', response);
             // Optionally, reset the form
             this.messageForm.patchValue({
-              message: '',
+              message: "",
               previousMessage: response.message, // Set previousMessage to the sent message
             });
             this.fetchMessages();
-
           },
           error: (err: any) => {
             // console.error('Error sending message:', err);
@@ -197,20 +199,21 @@ console.log("checking the emmpid",this.empid);
     // console.log('Creating Room');
     const roomInput = this.roomInput.nativeElement.value.trim();
     if (!roomInput) {
-      this.snackBar.open('Please enter room number', 'Close', {
+      this.snackBar.open("Please enter room number", "Close", {
         duration: 10000, // Duration in milliseconds
-        horizontalPosition: 'center',
-        verticalPosition: 'top'
+        horizontalPosition: "center",
+        verticalPosition: "top",
       });
       return;
     }
     // this.endCall=true;
     this.room_id = this.PRE + roomInput + this.SUF;
     this.peer = new Peer(this.room_id);
-    this.peer.on('open', (id: any) => {
+    this.peer.on("open", (id: any) => {
       // console.log('Peer Connected with ID: ', id);
       this.hideModal();
-      navigator.mediaDevices.getUserMedia({ video: true, audio: true }) // Updated getUserMedia
+      navigator.mediaDevices
+        .getUserMedia({ video: true, audio: true }) // Updated getUserMedia
         .then((stream: MediaStream) => {
           this.local_stream = stream;
           this.setLocalStream(this.local_stream);
@@ -220,12 +223,12 @@ console.log("checking the emmpid",this.empid);
           // console.log(err);
         });
       this.endCall = true;
-      this.notify('Waiting for peer to join.');
+      this.notify("Waiting for peer to join.");
     });
-    this.peer.on('call', (call: any) => {
+    this.peer.on("call", (call: any) => {
       if (this.local_stream) {
         call.answer(this.local_stream);
-        call.on('stream', (stream: MediaStream) => {
+        call.on("stream", (stream: MediaStream) => {
           this.setRemoteStream(stream);
           this.endCall = true;
         });
@@ -234,21 +237,21 @@ console.log("checking the emmpid",this.empid);
     });
   }
   confirmEndCall() {
-    if (confirm('Are you sure you want to end the call?')) {
+    if (confirm("Are you sure you want to end the call?")) {
       // Close the current Peer connection
       if (this.peer) {
         this.peer.disconnect();
         this.peer.destroy();
       }
-      
+
       // Optionally, stop local and remote streams
       if (this.local_stream) {
-        this.local_stream.getTracks().forEach(track => track.stop());
+        this.local_stream.getTracks().forEach((track) => track.stop());
       }
-      
-      this.room_id = '';
+
+      this.room_id = "";
       // User clicked OK, navigate to the specific URL
-      this.router.navigate(['dashboardemp/empmessage']); // Replace '/specific-url' with your desired URL
+      this.router.navigate(["dashboardemp/empmessage"]); // Replace '/specific-url' with your desired URL
     } else {
       // User clicked Cancel, do nothing or handle accordingly
     }
@@ -283,25 +286,26 @@ console.log("checking the emmpid",this.empid);
     // console.log('Joining Room');
     const room = this.roomInput.nativeElement.value.trim();
     if (!room) {
-      this.snackBar.open('Please enter room number', 'Close', {
+      this.snackBar.open("Please enter room number", "Close", {
         duration: 10000, // Duration in milliseconds
-        horizontalPosition: 'center',
-        verticalPosition: 'top'
+        horizontalPosition: "center",
+        verticalPosition: "top",
       });
       return;
     }
     this.room_id = this.PRE + room + this.SUF;
     this.hideModal();
     this.peer = new Peer();
-    this.peer.on('open', (id: string) => {
+    this.peer.on("open", (id: string) => {
       // console.log('Connected with Id: ' + id);
-      navigator.mediaDevices.getUserMedia({ video: true, audio: true }) // Updated getUserMedia
+      navigator.mediaDevices
+        .getUserMedia({ video: true, audio: true }) // Updated getUserMedia
         .then((stream: MediaStream) => {
           this.local_stream = stream;
           this.setLocalStream(this.local_stream);
-          this.notify('Joining peer');
+          this.notify("Joining peer");
           const call = this.peer!.call(this.room_id, stream); // Use non-null assertion here
-          call.on('stream', (stream: MediaStream) => {
+          call.on("stream", (stream: MediaStream) => {
             this.setRemoteStream(stream);
           });
           this.currentPeer = call;
@@ -312,28 +316,31 @@ console.log("checking the emmpid",this.empid);
     });
   }
 
-
   startScreenShare() {
     if (this.screenSharing) {
       this.stopScreenSharing();
     }
-    navigator.mediaDevices.getDisplayMedia({ video: true }).then((stream: MediaStream) => {
-      this.screenStream = stream;
-      const videoTrack = this.screenStream?.getVideoTracks()[0]; // Add a type guard here
-      if (videoTrack) {
-        videoTrack.onended = () => {
-          this.stopScreenSharing();
-        };
-        if (this.peer) {
-          const sender = this.currentPeer?.peerConnection.getSenders().find((s: any) => {
-            return s.track.kind == videoTrack.kind;
-          });
-          sender?.replaceTrack(videoTrack); // Add type guards here
-          this.screenSharing = true;
+    navigator.mediaDevices
+      .getDisplayMedia({ video: true })
+      .then((stream: MediaStream) => {
+        this.screenStream = stream;
+        const videoTrack = this.screenStream?.getVideoTracks()[0]; // Add a type guard here
+        if (videoTrack) {
+          videoTrack.onended = () => {
+            this.stopScreenSharing();
+          };
+          if (this.peer) {
+            const sender = this.currentPeer?.peerConnection
+              .getSenders()
+              .find((s: any) => {
+                return s.track.kind == videoTrack.kind;
+              });
+            sender?.replaceTrack(videoTrack); // Add type guards here
+            this.screenSharing = true;
+          }
+          // console.log(this.screenStream);
         }
-        // console.log(this.screenStream);
-      }
-    });
+      });
   }
 
   stopScreenSharing() {
@@ -341,9 +348,11 @@ console.log("checking the emmpid",this.empid);
     const videoTrack = this.local_stream?.getVideoTracks()[0]; // Add a type guard here
     if (videoTrack) {
       if (this.peer) {
-        const sender = this.currentPeer?.peerConnection.getSenders().find((s: any) => {
-          return s.track.kind == videoTrack.kind;
-        });
+        const sender = this.currentPeer?.peerConnection
+          .getSenders()
+          .find((s: any) => {
+            return s.track.kind == videoTrack.kind;
+          });
         sender?.replaceTrack(videoTrack); // Add type guards here
       }
       this.screenStream?.getTracks().forEach((track: MediaStreamTrack) => {
@@ -355,7 +364,7 @@ console.log("checking the emmpid",this.empid);
 
   private loadScript(scriptUrl: string): Promise<void> {
     return new Promise<void>((resolve, reject) => {
-      const script = this.renderer.createElement('script');
+      const script = this.renderer.createElement("script");
       script.src = scriptUrl;
       script.onload = () => {
         resolve();
@@ -372,7 +381,7 @@ console.log("checking the emmpid",this.empid);
 
     if (localVideoElement.requestFullscreen) {
       if (!document.fullscreenElement) {
-        localVideoElement.requestFullscreen().catch(err => {
+        localVideoElement.requestFullscreen().catch((err) => {
           // console.log(`Error attempting to enable full-screen mode: ${err.message}`);
         });
       } else {
@@ -380,5 +389,4 @@ console.log("checking the emmpid",this.empid);
       }
     }
   }
-
 }
